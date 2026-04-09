b2306c99f7b6ff5031b444cd2a1b7ba4779d1968
Peng Ding <pding.dp@foxmail.com>
2026-04-09 22:14:17 +0800

    chore: bump version to 0.4.0

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 # /// zerodep
-# version = "0.3.0"
+# version = "0.4.0"
 # deps = ["dotenv", "yaml", "jsonc"]
 # tier = "subsystem"
 # category = "data"
--- httpclient/httpclient.py
+++ httpclient/httpclient.py
@@ -1,5 +1,5 @@
 # /// zerodep
-# version = "0.3.0"
+# version = "0.4.0"
 # deps = []
 # tier = "subsystem"
 # category = "network"
